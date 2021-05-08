b5cfbbbdd0004ec3f247af99e5137a781f9183f8
Katia <katiao_@hotmail.co.uk>
2021-05-08 13:05:29 +0200

    add toggle functionality and grid layout to Blog page

--- src/Pages/Blog/blog.jsx
+++ src/Pages/Blog/blog.jsx
@@ -1,16 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import './blog.scss';
 import { articles } from './data.js';
 import Article from '../../components/Article/article';
 
 function Blog() {
+	const [theme, setTheme] = useState('dark-theme');
+
+	const toggleTheme = () => {
+		if (theme === 'dark-theme') {
+			setTheme('light-theme');
+		} else {
+			setTheme('dark-theme');
+		}
+	};
+
+	useEffect(() => {
+		document.documentElement.className = theme;
+	}, [theme]);
+
 	return (
-		<div>
-			<nav>
-				<div className='title-section'>
-					<h1>Katia's Blog Page</h1>
-					<button className='btn'>toggle</button>
-				</div>
-			</nav>
+		<div className='blog-section'>
+			<h1>Katia's Blog</h1>
+			<button className='btn' onClick={toggleTheme}>
+				toggle
+			</button>
+
 			<section className='articles'>
 				{articles.map(item => {
 					return <Article key={item.id} {...item} />;
