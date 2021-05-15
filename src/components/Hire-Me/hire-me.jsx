007051da5a2c8da5040117683c638d32f5b99d9b
Katia <katiao_@hotmail.co.uk>
2021-05-15 17:03:43 +0200

    change Hire Me section

--- src/components/Hire-Me/hire-me.jsx
+++ src/components/Hire-Me/hire-me.jsx
@@ -5,7 +5,12 @@ import Heading2 from '../Heading-2/heading-2';
 function HireMe() {
 	return (
 		<section className='hire-me'>
-			<Heading2 text='Have a Project in Mind?' />
+			<Heading2 text='Hire Me' />
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt
+				distinctio nostrum porro asperiores! <br />
+				Quidem blanditiis eveniet harum cum possimus iste.
+			</p>
 			<Button
 				btnClass='grey-button'
 				text='Hire Me'
